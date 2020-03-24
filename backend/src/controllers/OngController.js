3977ff0014534b2c2ccbb21b1a9ca807fd65030d
pedroicandido <pedroicandido1@gmail.com>
2020-03-24 15:51:09 -0300

    Back-end da aplicação

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,25 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+module.exports = {
+    async store(req, res){
+
+        const id = crypto.randomBytes(4).toString('HEX');
+        const {name, email, whatsapp, city, uf} = req.body;
+        
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf
+        })
+        return res.json({id})
+    },
+
+    async index(req, res){
+        const ongs = await connection('ongs').select('*');
+        return res.json(ongs)
+    }
+}
